091a1f99573d9a76da6b8cefac37d73e93566d9b
raj0311 <105967687+raj0311@users.noreply.github.com>
2023-08-13 17:07:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// script.js
+const celsiusField = document.querySelector("#celsius-value");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#temp-type");
+
+window.addEventListener("load", () => {
+  degree.value = "";
+  celsiusField.innerHTML = "";
+});
+
+if(degree.value === ""){
+  convertBtn.setAttribute("disabled","");
+  setTimeout(() => {
+    convertBtn.removeAttribute('disabled');
+  }, 4000);
+}
+
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convertToCelsius();
+  convertBtn.innerHTML = "<span class='icon'><i class='fa fa-spinner fa-spin'></i> Converting...</span>";
+  setTimeout(() => {
+    convertBtn.innerHTML ="<span>Convert</span>"
+  }, 1000);
+});
+
+function convertToCelsius() {
+  let inputValue = degree.value;
+  
+  setTimeout( () => {
+    if (tempType.value === "celsius") {
+      const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+      celsiusField.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;C`;
+    } else if (tempType.value === "fahrenheit") {
+      const CelciusToFarenheit = inputValue*(9/5)+32;
+      celsiusField.innerHTML = `${CelciusToFarenheit.toFixed(3)} &deg;F`;
+    }
+  }, 1200)
+}
